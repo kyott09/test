9f4c8ba34b94773d4b3fe436c6b78b4205e08527
kyott09 <tobiasdinelli@gmail.com>
2025-05-08 01:03:19 -0300

    1

--- api/main.py
+++ api/main.py
@@ -12,9 +12,6 @@ ARCHIVO = "usuarios.json"
 def read_root():
     return {"message": "Hello World"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 # ─── Modelos ─────────────────────────────────────────────────────────
 
